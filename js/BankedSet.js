47da8a1e9478ae0d2eb049c075a15339e81690ce
Oliver Foster <oliver.foster@kineo.com>
2025-07-30 05:34:36 +0100

    Breaking: Updated to version 2 of scoring (fixes #3)

--- js/BankedSet.js
+++ js/BankedSet.js
@@ -1,72 +1,90 @@
 import Logging from 'core/js/logging';
-import ModifierSet from 'extensions/adapt-contrib-modifiers/js/ModifierSet';
 import Bank from './Bank';
+import {
+  LifecycleSet,
+  StateSetModelChildren
+} from 'extensions/adapt-contrib-scoring/js/adapt-contrib-scoring';
 
-export default class BankedSet extends ModifierSet {
+export default class BankedSet extends LifecycleSet {
 
   initialize(options = {}) {
-    this._banks = {};
     super.initialize({
       ...options,
       _type: 'banking'
     });
-    if (this.isAwaitingChildren) {
-      Logging.error(`Models cannot be added dynamically when using banking. Please check config for ${this.modelId}.`);
-      return;
-    }
+    if (!this.isAwaitingChildren) return;
+    Logging.error(`Models cannot be added dynamically when using banking. Please check config for ${this.modelId}.`);
   }
 
   /**
-   * @override
+   * Fetch the config object from the set model.
+   * @returns {Object}
    */
-  setupModels() {
-    if (!this.isEnabled) return;
-    let models = [];
-    const savedModels = this._getSavedModels();
-    if (savedModels) {
-      models = savedModels
-    } else if (this._split) {
-      this._split.forEach((count, index) => {
-        const bankId = index + 1;
-        const bank = this._banks[bankId] ?? new Bank(bankId, this.originalModels, parseInt(count));
-        bank.selectModels();
-        models.push(...bank.models);
-        this._banks[bankId] = bank;
-      });
-    }
-    this.models = models;
-    this._saveState();
+  get config() {
+    return this.model.get('_banking');
   }
 
   /**
-   * @override
+   * Create a state object which saves and restores the set models.
+   * @returns {StateSetModelChildren}
    */
+  get state() {
+    if (this.isIntersectedSet) return;
+    return (this._state = this._state || new StateSetModelChildren({ set: this }));
+  }
+
+  /** @override */
   get order() {
-    return 1;
+    // 200 is less than randomise (300) and scoringAssessment (500) but greater than AdaptModelSet (< 100)
+    return 200;
   }
 
   /**
-   * @override
+   * Returns whether the set is enabled
+   * @returns {boolean}
    */
-  _initConfig() {
-    this._config = this.model.get('_banking');
-    this._split = this.isEnabled ? this._config?._split?.split(',') : null;
+  get isEnabled() {
+    return this.config?._isEnabled ?? false;
   }
 
   /**
-   * @extends
+   * Returns the number of items to pick from each bank.
+   * @returns {number[]|null}
    */
-  _setupListeners() {
-    super._setupListeners();
-    this.listenTo(this.model, 'change:_banking', this.onModelConfigChange);
+  get split() {
+    return this.isEnabled
+      ? this.config._split.split(',').map(Number)
+      : null;
   }
 
-  /**
-   * @extends
-   */
-  onModelConfigChange() {
-    super.onModelConfigChange();
+  /** @override */
+  onInit() {
+    if (!this.isEnabled) return;
     this._banks = {};
+  }
+
+  /** @override */
+  async onRestore() {
+    if (!this.isEnabled) return false;
+    if (!this.state.restore()) return false;
+    await super.onRestore();
+    return true;
+  }
+
+  /** @override */
+  async onStart() {
+    if (!this.split) return;
+    const models = [];
+    this.split.forEach((count, index) => {
+      const bankId = index + 1;
+      const bank = this._banks[bankId] ?? new Bank(bankId, this.models, parseInt(count));
+      bank.selectModels();
+      bank.models.forEach(model => models.push(model));
+      this._banks[bankId] = bank;
+    });
+    this.model.getChildren().reset(models);
+    this.state.save();
+    await super.onStart();
   }
 
 }
